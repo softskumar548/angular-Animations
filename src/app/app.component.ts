e8079fcc157618dde68394a2407cb7bcd86965f8
Siva Kumar <anveti.sivakumar@ntint.com>
2024-06-26 19:11:03 +0530

    tile_animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { trigger, transition, state, animate, style, AnimationEvent } from '@angular/animations';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
+  animations: [
+    trigger('hoverState', [
+      state('hover', style({
+        maxHeight: '472px',
+        opacity: 1,
+        background: 'linear-gradient(to bottom, #ffffff, #add8e6)',
+        overflow: 'visible'
+      })),
+      state('default', style({
+        maxHeight: '294px',
+        opacity: 0.8,
+        backgroundColor: '#fff',
+        overflow: 'hidden'
+      })),
+      transition('default <=> hover', [
+        animate('0.2s ease-in-out')
+      ])
+    ]),
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'my-angular-project';
+  hover: boolean = false;
 }
